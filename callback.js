a67b1926fa9817fe547c64b5928ac73fe71652e1
Nick <pickme1152019@gmail.com>
2019-03-07 04:35:25 +0000

    changed to for each

--- callback.js
+++ callback.js
@@ -1,14 +1,28 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  let i = 0;
+  arr.forEach(function(x){
+
+    if(x == "Waldo"){
+      found(i)
     }
-  }
+    i++
+  });
 }
 
+
 function actionWhenFound(i) {
-  console.log("Found Waldo at index " + i + "!!!");
+  console.log("Found Waldo at index " + i);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+// // before
+// for (let i=0; i<items.length; i++) {
+//   copy.push(items[i]);
+// }
+
+// // after
+// items.forEach(function(item){
+//   copy.push(item);
+// });
